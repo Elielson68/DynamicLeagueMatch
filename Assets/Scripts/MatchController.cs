e38cbb397e44446e36b695b5d84f4ce2f8d54af6
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-04-18 16:44:41 -0300

    feat: ajusta criação de novas Matchs, lógica final finalizada a primeira parte para teste

--- Assets/Scripts/MatchController.cs
+++ Assets/Scripts/MatchController.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Linq;
 using DG.Tweening;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MatchController : MonoBehaviour
 {
-    public float Distance = -4;
+    public MatchController MatchPrefab;
+    public float Distance = 2;
     public Vector3 FinalMatchPoint;
     public Vector3[] ChildsPositions;
     public int[] MatchPlayersIndex = new int[2];
     private LineRenderer[] _linesRenderers;
+    private bool _animationFinish;
 
     IEnumerator Start()
     {
@@ -20,19 +23,27 @@ public class MatchController : MonoBehaviour
         }
 
         yield return new WaitForEndOfFrame();
+
+        UpdatePositions();
+        CreateLines();
+
+        yield return new WaitUntil(() => _animationFinish);
+
+        CreateNewMatch();
+    }
+
+    public void UpdatePositions()
+    {
         _linesRenderers = GetComponentsInChildren<LineRenderer>();
         var listAux = transform.GetComponentsInChildren<Image>().ToList().ConvertAll(x => x.rectTransform);
         ChildsPositions = listAux.ConvertAll(img => img.position).ToArray();
-        _linesRenderers.ToList().ForEach(x => Debug.Log(x.name, x.gameObject));
-        CreateLines();
     }
 
     [ContextMenu("Debug Positions")]
-    public void DebugPositions()
+    public void CreatePositionsDebug()
     {
-        var listAux = transform.GetComponentsInChildren<Image>().ToList().ConvertAll(x => x.rectTransform);
-        listAux.RemoveAt(0);
-        listAux.ForEach(img => Debug.Log(img.position));
+        UpdatePositions();
+        CreateLines();
     }
 
     [ContextMenu("Create Lines")]
@@ -48,8 +59,6 @@ public class MatchController : MonoBehaviour
         int p1 = MatchPlayersIndex[0];
         int p2 = MatchPlayersIndex[1];
 
-        FinalMatchPoint = ChildsPositions[p1] + ChildsPositions[p2] / 2;
-
         var firstChildPositions = GetChildPositions(ChildsPositions[p1], ChildsPositions[p2]);
         var secondChildPositions = GetChildPositions(ChildsPositions[p2], ChildsPositions[p1]);
 
@@ -57,15 +66,26 @@ public class MatchController : MonoBehaviour
         MoveLinesFoward(_linesRenderers[p2], secondChildPositions);
     }
 
+    public float GetMidPointBetweenPlayers()
+    {
+        UpdatePositions();
+
+        int p1 = MatchPlayersIndex[0];
+        int p2 = MatchPlayersIndex[1];
+
+        Vector3 middlePosition = (ChildsPositions[p1] + ChildsPositions[p2]) / 2;
+        return middlePosition.y;
+    }
+
     private Vector3[] GetChildPositions(Vector3 firstChild, Vector3 secondChild)
     {
         Vector3[] positions = new Vector3[3];
 
         Vector3 startPosition = firstChild;
-        Vector3 endPosition = new Vector3(Distance, startPosition.y, startPosition.z);
+        Vector3 endPosition = new Vector3(startPosition.x + Distance, startPosition.y, startPosition.z);
 
         Vector3 middlePosition = (firstChild + secondChild) / 2;
-        middlePosition.x = Distance;
+        middlePosition.x = endPosition.x;
 
         positions[0] = startPosition;
         positions[1] = endPosition;
@@ -76,50 +96,98 @@ public class MatchController : MonoBehaviour
 
     private void MoveLinesFoward(LineRenderer line, Vector3[] positions)
     {
+        _animationFinish = false;
         line.SetPosition(0, positions[0]);
-        Vector3 moveAux = positions[0];
+        Vector3 auxPos = positions[0];
 
-        DOTween.To(x => { moveAux.x = x; line.SetPosition(1, moveAux); line.SetPosition(2, moveAux); }, moveAux.x, positions[1].x, 2f)
+        DOTween.To(x => { auxPos.x = x; line.SetPosition(1, auxPos); line.SetPosition(2, auxPos); }, auxPos.x, positions[1].x, 2f)
         .OnComplete(() =>
         {
             line.SetPosition(1, positions[1]);
 
-            moveAux = positions[1];
-            DOTween.To(y => { moveAux.y = y; line.SetPosition(2, moveAux); }, moveAux.y, positions[2].y, 2f);
+            auxPos = positions[1];
+            DOTween.To(y => { auxPos.y = y; line.SetPosition(2, auxPos); }, auxPos.y, positions[2].y, 2f)
+            .OnComplete(() =>
+            {
+                FinalMatchPoint = auxPos;
+                _animationFinish = true;
+            });
         });
+    }
+
+    private void CreateNewMatch()
+    {
+        Vector3 auxFinalMatch = FinalMatchPoint;
+
+        if (transform.childCount == 1)
+        {
+            return;
+        }
+
+        var match = Instantiate(MatchPrefab, auxFinalMatch, Quaternion.identity, transform.parent);
+
+        if (transform.childCount == 2)
+        {
+            Destroy(match.transform.GetChild(1).gameObject);
+            auxFinalMatch.y += 0.5f;
+            auxFinalMatch.x += 0.5f;
+            match.transform.position = auxFinalMatch;
+            return;
+        }
+
+        auxFinalMatch.y += 1.5f;
+        auxFinalMatch.x += 0.5f;
+        match.transform.position = auxFinalMatch;
+        match.MatchPrefab = MatchPrefab;
     }
 
     private bool IsValidMatch()
     {
+        AutomaticConfigurePlayers();
+
         if (MatchPlayersIndex.Length > 2)
         {
             Debug.LogError("Só é possível realizar o Match entre 2 jogadores!");
             return false;
         }
 
+        if (ExistSameElement())
+        {
+            Debug.LogError("Não é possível realizar o Match com elementos iguais!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool ExistSameElement()
+    {
         var l1 = MatchPlayersIndex.ToList();
 
-        foreach (var x in MatchPlayersIndex)
+        for (int i = 0; i < MatchPlayersIndex.Length; i++)
         {
-            foreach (var y in MatchPlayersIndex)
+            for (int ii = 0; ii < MatchPlayersIndex.Length; ii++)
             {
-                Debug.Log($"x: {x} i: {l1.IndexOf(x)} | y: {y} i: {l1.IndexOf(y)}");
-
-                if (x == y && l1.IndexOf(x) != l1.IndexOf(y))
+                if (MatchPlayersIndex[i] == MatchPlayersIndex[ii] && i != ii)
                 {
-                    Debug.LogError("Não é possível realizar o Match com elementos iguais!");
-                    return false;
+                    return true;
                 }
             }
         }
 
-        AutomaticConfigurePlayers();
-        return true;
+        return false;
     }
 
     private void AutomaticConfigurePlayers()
     {
         if (MatchPlayersIndex.Length < 2)
+        {
+            MatchPlayersIndex = new int[2];
+            MatchPlayersIndex[0] = 0;
+            MatchPlayersIndex[1] = 1;
+        }
+
+        if (MatchPlayersIndex.Length == 2 && ExistSameElement())
         {
             MatchPlayersIndex[0] = 0;
             MatchPlayersIndex[1] = 1;
